ccdaedc427a886518df138268241e08f123ce712
Mahmoud elmasry <mahmoudelmasry852001@gmail.com>
2025-12-15 17:23:43 +0200

    Update 03_Load_Fact_Dimensions.sql

--- SQL/03_Load_Fact_Dimensions.sql
+++ SQL/03_Load_Fact_Dimensions.sql
@@ -1,2 +1,9 @@
-placeholder
+INSERT INTO dw.DimPatient (PatientID, PatientName, Gender, City)
+SELECT PatientID, PatientName, Gender, City
+FROM stg.Patients;
+
+INSERT INTO dw.DimDoctor (DoctorID, DoctorName, Specialization, Department)
+SELECT DoctorID, DoctorName, Specialization, Department
+FROM stg.Doctors;
+
 
